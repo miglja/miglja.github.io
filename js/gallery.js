d1277234c23d0eb83798de3bd566f0d3434ee23a
Michael Jackson <miglja619@gmail.com>
2025-01-12 01:38:24 -0500

    Finish the Javascript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,37 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    //let thumbnails = document.querySelector("#gallery-thumbs")
+    //                         .querySelectorAll("img");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img")
+
+    let mainImage = document.querySelector("#gallery-photo img");
+
+    // Image info to be updated
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            // set clicked image as main image.
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            let newImageAlt = thumbnail.dataset.alt;
+            mainImage.setAttribute("src", newImageSrc);
+            mainImage.setAttribute("alt", newImageAlt)
+
+            //Change which image is current.
+            let currentClass = "current";
+            document.querySelector(".current")
+                    .classList
+                    .remove(currentClass);
+            thumbnail.parentNode
+                    .classList
+                    .add(currentClass);
+
+            // Update image info
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
 }
